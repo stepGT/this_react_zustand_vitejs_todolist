b85465d32b3de352e947f7cd7f157e725a53df94
Andrey Zuev <stepgt@mail.ru>
2022-10-19 14:38:20 +1000

    Added reset && common styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './views/App';
 
+import './views/styles/reset.scss';
+import './views/styles/common.scss';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styles from './index.module.scss';
 
 const App: React.FC = () => {
-  return <article>test</article>;
+  return (
+    <article className={styles.article}>
+      <h1 className={styles.title}>ToDo App</h1>
+      <section className={styles.section}></section>
+      <section className={styles.section}></section>
+    </article>
+  );
 };
 
 export default App;
