7f1b87074c18dd0a3a2f99a0a15a27a568f8e1ae
Andrey Zuev <stepgt@mail.ru>
2022-10-25 09:23:37 +1000

    Added local storage middleware

--- src/data/stores/useToDoStore.ts
+++ src/data/stores/useToDoStore.ts
@@ -1,5 +1,4 @@
-import create from 'zustand';
-import { devtools } from 'zustand/middleware';
+import create, { State, StateCreator } from 'zustand';
 import { generateID } from '../helpers';
 
 interface Task {
@@ -15,9 +14,38 @@ interface ToDoStore {
   removeTask: (id: string) => void;
 }
 
+function isToDoStore(object: any): object is ToDoStore {
+  return 'tasks' in object;
+}
+
+const localStorageUpdate =
+  <T extends State>(config: StateCreator<T>): StateCreator<T> =>
+  (set, get, api) =>
+    config(
+      (nextState, ...args) => {
+        if (isToDoStore(nextState)) {
+          window.localStorage.setItem('tasks', JSON.stringify(nextState.tasks));
+        }
+        set(nextState, ...args);
+      },
+      get,
+      api,
+    );
+
+const getCurrentState = () => {
+  try {
+    const currentState = JSON.parse(window.localStorage.getItem('tasks') || '[]') as Task[];
+    return currentState;
+  } catch (err) {
+    window.localStorage.setItem('tasks', '[]');
+  }
+
+  return [];
+};
+
 export const useToDoStore = create<ToDoStore>(
-  devtools((set, get) => ({
-    tasks: [],
+  localStorageUpdate((set, get) => ({
+    tasks: getCurrentState(),
     createTask: (title) => {
       const { tasks } = get();
       const newTask = {
